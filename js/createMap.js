b8c9e5a96d46350e07f91d9f7ca4ac6fec1d75f3
frzbrmnd <frzbrmnd@gmail.com>
2021-07-01 00:47:15 +0430

    step 1: create map for admin and users

--- js/createMap.js
+++ js/createMap.js
@@ -1,1 +1,39 @@
+var osm = new ol.layer.Tile({
+    source: new ol.source.OSM(),
+})
 
+var view = new ol.View({
+    projection: 'EPSG:4326',
+    center: [51.391098, 35.700927],
+    zoom: 12,
+});
+
+//disabe rotation
+var interactions = ol.interaction.defaults({altShiftDragRotate:false, pinchRotate:false});
+//make attribution collapsible when width<600px 
+var attribution = new ol.control.Attribution({
+  collapsible: false,
+});
+function checkSize() {
+  var small = map.getSize()[0] < 600;
+  attribution.setCollapsible(small);
+  attribution.setCollapsed(small);
+}
+
+
+
+var controls = ol.control.defaults({
+    rotate: false,
+    attribution: false,
+}).extend([attribution]);
+
+var map = new ol.Map({
+    layers: [osm],
+    target: 'map',
+    view: view,
+    interactions: interactions,
+    controls: controls,
+});
+
+window.addEventListener('resize', checkSize);
+checkSize();
--- users/drivers.php
+++ users/drivers.php
@@ -1,1 +1,19 @@
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Tracking Food Trucks</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <script src="./js/ol.js"></script>
+        <link rel="shortcut icon" href="#"> <!--for netbeans favicon error-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>-->
+        <link rel="stylesheet" href="./css/ol.css" type="text/css">
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>    
+        <div id="map" class="map container-fluid"></div>
+        <script src="./js/createMap.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    </body>
+</html>
